832d9e082ce79f3efd7a40ce85af208ddb108195
saent-x <torsxperience@outlook.com>
2024-12-15 23:34:31 +0000

    updated hero content

--- src/components/hero_content.rs
+++ src/components/hero_content.rs
@@ -8,15 +8,15 @@ pub fn HeroContent() -> Element{
     rsx!{
         div { 
             class: "flex flex-col w-[100%] items-end",
-            img{ src: TOR_IMAGE, class: "h-36 w-36 rounded-full border-4 border-black" }
+            img{ src: TOR_IMAGE, class: "h-36 w-36 rounded-md border-2 shadow border-black" }
         }
         div { 
             h1 { 
-                class: "text-bold text-2xl bg-gray-800 text-white p-1 px-2 rounded w-fit",
+                class: "text-bold text-xl bg-gray-800 text-white p-1 px-2 rounded w-fit shadow",
                 "Hello 👋🏾, I'm Vangerwua Tor" 
             }
             p {
-                class: "mt-2 text-justify text-base bg-gray-800 text-white p-2 rounded w-fit",
+                class: "mt-2 text-justify text-sm bg-gray-800 text-white p-2 rounded w-fit shadow",
                 "
                 Welcome to my digital space! I'm excited to share my life experiences and professional 
                 journey as a Software Engineer. Dive in to explore my projects, tips, and the latest 
