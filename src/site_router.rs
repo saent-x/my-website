8ea2ebb424abf325092f6079e4c5c738bf32d479
saent-x <torsxperience@outlook.com>
2024-12-21 14:16:50 +0000

    ignored variant postfix warning

--- src/site_router.rs
+++ src/site_router.rs
@@ -1,13 +1,22 @@
-use crate::pages::{home::Home, blog::Blog};
+use crate::pages::home::HomePage;
+use crate::pages::blog::BlogPage;
+use crate::pages::blogpost::BlogPostPage;
+
 use crate::components::layout::Layout;
 use dioxus::prelude::*;
 
+
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
+#[allow(clippy::enum_variant_names)]
 pub enum SiteRoute {
     #[layout(Layout)]
     #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
+    HomePage {},
+
+    #[route("/blog_posts")]
+    BlogPage { },
+    
+    #[route("/blog/:blog_post_id")]
+    BlogPostPage {blog_post_id: u32}
 }
