a95e86e7dedbcf9313e61d76327f3570a6fde0db
saent-x <torsxperience@outlook.com>
2024-12-22 02:18:18 +0000

    tested converting markdown to html

--- src/pages/blogpost.rs
+++ src/pages/blogpost.rs
@@ -1,24 +1,23 @@
 use dioxus::{logger::tracing::info, prelude::*};
-use std::path::Path;
 use site_core::services::blog_post_service::BlogPost;
+use crate::prelude::*;
+
 
 #[component]
 pub fn BlogPostPage(blog_post_id: u32) -> Element {
-    let md_string = site_core::util::read_md_file(Path::new(""))
-        .unwrap_or_else(|e| {
-            info!("[ERROR]: an error ocurred while loading markdown file; {}", e);
+    // get post from db based on id
+    info!("[INFO]: blog_post id -> {}", blog_post_id);
 
-            String::from("### 🤦🏾‍♂️ An unfortunate circumstance occurred. Try again.")
-        });
-    let blog_post = BlogPost::new("John", "10-12-2024", md_string);
+    let blog_post = BlogPost::new(1, "John", "10-12-2024", "sample title", "sample description", "#programming", SAMPLE_MD.to_string());
     
     // convert post to html
     let html_repr_post = blog_post.convert_content_to_html();
 
     rsx!(
         div { 
-            class: "",
+            id: "blog_post",
+            class: "no-tailwindcss w-[55%]",
             dangerous_inner_html: html_repr_post
-         }
+        }
     )
 }
