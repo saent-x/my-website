d89c62c4bd40b81bf8c89a9a544ccfd07eed71c4
Vangerwua Tor <45785419+saent-x@users.noreply.github.com>
2024-12-29 04:00:24 +0000

    moved response gen & uuid gen fn to util

--- server/src/util.rs
+++ server/src/util.rs
@@ -1,19 +1,48 @@
-use std::path::Path;
 use std::error::Error;
 use std::fs;
+use std::path::Path;
 
+use crate::prelude::ResponseStatusType;
+use serde::Serialize;
+use serde_json::json;
+use serde_json::Value;
 use uuid::Uuid;
 
-
-pub fn read_md_file(path: &Path) -> Result<String, Box<dyn Error>>{
+pub fn read_md_file(path: &Path) -> Result<String, Box<dyn Error>> {
     let md_string = fs::read_to_string(path.to_str().unwrap())?;
 
     Ok(md_string)
 }
 
-pub fn gen_uuid() -> String{
+pub fn gen_uuid() -> String {
     let mut uuid_buffer: [u8; 45] = Uuid::encode_buffer();
     let bp_uuid: &mut str = Uuid::new_v4().simple().encode_lower(&mut uuid_buffer);
 
     bp_uuid.to_string()
+}
+
+pub fn gen_response<T>(response_status_type: ResponseStatusType, data: T) -> Value
+where
+    T: Serialize,
+{
+    match response_status_type {
+        ResponseStatusType::Success(code) => {
+            json!(
+                {
+                    "status": "success",
+                    "code": code,
+                    "data":  data
+                }
+            )
+        }
+        ResponseStatusType::Error(code) => {
+            json!(
+                {
+                    "status": "error",
+                    "code": code,
+                    "data":  data
+                }
+            )
+        }
+    }
 }
